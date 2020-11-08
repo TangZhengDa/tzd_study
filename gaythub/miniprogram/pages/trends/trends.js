4b2128729a43b3cec673bf48a3009e633eb88367
TangZhengDa <695698426@qq.com>
2020-11-08 23:13:11 +0800

    2020-11-08

---
+++ gaythub/miniprogram/pages/trends/trends.js
@@ -0,0 +1,89 @@
+// miniprogram/pages/trends/trends.js
+const timeRange = [
+  { label: 'Daily', value: 'Daily' },
+  { label: 'Weekly', value: 'Weekly' },
+  { label: 'Monthly', value: 'Monthly' }
+]
+
+const languages = [
+  'All',
+  'C', 'CSS', 'C#', 'C++',
+  'Dart', 'Dockerfile',
+  'Erlang',
+  'Gradle', 'Go',
+  'HTML', 'Haskell',
+  'Java', 'JavaScript', 'JSON', 'Julia',
+  'Kotlin',
+  'MATLAB',
+  'Python', 'PHP',
+  'R', 'Ruby', 'Rust',
+  'Shell', 'SQL', 'Swift',
+  'TeX',
+  'Vue'
+].map(it => ({ label: it, value: it }))
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    theme: "",
+    trends: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
